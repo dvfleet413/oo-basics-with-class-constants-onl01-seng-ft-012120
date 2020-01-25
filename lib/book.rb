cbbc852188a2d812704892a01338323cde6075cd
David Van Fleet <dvfleet413@gmail.com>
2020-01-25 04:43:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
+  attr_reader :title, :genre
   
   GENRES = []
 
@@ -13,10 +13,6 @@ class Book
     GENRES << genre
   end
   
-  def genre
-    @genre
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
